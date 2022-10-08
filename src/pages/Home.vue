5dfd3ee59bc010d6509d579157a4ad6d47e4d5e1
phuet1 <112240579+phuet1@users.noreply.github.com>
2022-10-08 21:30:19 +0700

    Add files via upload

---
+++ src/pages/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  
+  <section class="home">
+
+    <div class="image">
+        <img src="images/mob.png" alt="">
+    </div>
+
+    <div class="content">
+        <h3> hello, Phuet Phongkheaw </h3>
+        <span> great power comes with great responsibility </span>
+        <p> Hi </p>
+        <a href="about.html" class="btn"> about me <i class="fas fa-user"></i> </a>
+    </div>
+
+</section>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+export default {
+  components: { Navbar },
+
+}
+</script>
+
+<style>
+
+</style>
